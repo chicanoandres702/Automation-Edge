29a7cf004b83a1f1a067691fcbef66e8318cda78
Andrew Moreno <andrew.moreno.5691@gmail.com>
2026-03-15 18:43:25 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,8 @@
-
 export const firebaseConfig = {
-  apiKey: "process.env.NEXT_PUBLIC_FIREBASE_API_KEY",
-  authDomain: "process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN",
-  projectId: "process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID",
-  storageBucket: "process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET",
-  messagingSenderId: "process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID",
-  appId: "process.env.NEXT_PUBLIC_FIREBASE_APP_ID"
+  "projectId": "studio-2706326544-19387",
+  "appId": "1:236295528765:web:4221cc05b28b09b0242554",
+  "apiKey": "AIzaSyC-JfOvcdSZHh6eG-UDIQvoIBO6kYoda14",
+  "authDomain": "studio-2706326544-19387.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "236295528765"
 };
